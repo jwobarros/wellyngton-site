0ad88a0aaa4bc243519645b1fb1da144fea57d76
Johnnatan Barros <johnnatanwillian@hotmail.com>
2020-02-27 15:44:44 -0400

    Added app config

--- blog/apps.py
+++ blog/apps.py
@@ -1,5 +1,8 @@
 from django.apps import AppConfig
-
+from watson import search as watson
 
 class BlogConfig(AppConfig):
     name = 'blog'
+    def ready(self):
+        Post = self.get_model("Post")
+        watson.register(Post)
